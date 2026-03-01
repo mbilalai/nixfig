a2fd28458b7b1bab877f47427a2d987e3e9055bc
mbilalai <mbilalai@protonmail.ch>
2026-03-01 14:49:03 +1100

    brute 0.0.1

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Modular, Reproducible, and Cross-Platform Nix Configs";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nur.url = "github:nix-community/NUR";
+  };
+  outputs = { self, nixpkgs, nur, ... } @ inputs:
+  let
+    x86_64-linux = "x86_64-linux";
+   
+    username = "mbk";
+    mkPkgs = system:
+      import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [
+          (final: prev: {
+            unstable = import inputs.nixpkgs-unstable {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          })
+        ]  ++ [ nur.overlays.default ];
+      };
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { 
+        inherit username; 
+        inputs = inputs; 
+        pkgs-stable = mkPkgs x86_64-linux;
+      };
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
