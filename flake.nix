0d40cce0df39392291b3a0435496378e250fadba
mbilalai <mbilalai@protonmail.ch>
2026-06-15 19:12:51 +1000

    cleanup

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Modular, Reproducible, and Cross-Platform Nix Configs";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nur = {
